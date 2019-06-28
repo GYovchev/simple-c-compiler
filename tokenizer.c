629823e0307c966cf222685de18a75a2aa29ed6e
Georgi Yovchev <georgi.yovchev@sciant.com>
2019-06-28 18:31:24 +0300

    add dynamic string to utils

--- tokenizer.c
+++ tokenizer.c
@@ -8,13 +8,18 @@
 #define NULL 0
 #endif
 
-#define NEXT_CHAR(C) C = next_char(); if (C == NULL) return NULL;
+#define NEXT_CHAR(C) \
+  C = next_char();   \
+  if (C == NULL)     \
+    return NULL;
 
 static int line = 0;
 
-char* next_char() {
-  char* c = get_char(NULL);
-  if(*c == '\n') line++;
+char *next_char()
+{
+  char *c = get_char(NULL);
+  if (*c == '\n')
+    line++;
   return c;
 }
 
@@ -23,28 +28,28 @@ Token *next_token()
   char *c = next_char();
   if (is_valid_var_name(c) == 1)
   {
-      while (is_valid_var_name(c))
+    while (is_valid_var_name(c))
+    {
+      printf("%c", *c);
+      NEXT_CHAR(c)
+      if (is_whitespace(c))
       {
-        printf("%c", *c);
-        NEXT_CHAR(c)
-        if(is_whitespace(c))
+        while (is_whitespace(c))
         {
-          while(is_whitespace(c)) 
+          printf("%c", *c);
+          NEXT_CHAR(c)
+        }
+
+        if (is_valid_var_name(c) == 1)
+        {
+          while (is_valid_var_name(c))
           {
             printf("%c", *c);
             NEXT_CHAR(c)
           }
-
-          if(is_valid_var_name(c) == 1) 
-          {
-            while (is_valid_var_name(c))
-            {
-              printf("%c", *c);
-              NEXT_CHAR(c)
-            }
-          }
         }
       }
+    }
   }
   else if (is_digit(c))
   {
--- utils.c
+++ utils.c
@@ -2,74 +2,96 @@
 
 void d(char *c)
 {
-    printf("%s", c);
+  printf("%s", c);
 }
 
 char is_alpha(char *c)
 {
-    if ((*c >= 'a' && *c <= 'z') || (*c >= 'A' && *c <= 'Z'))
-        return 1;
-    return 0;
+  if ((*c >= 'a' && *c <= 'z') || (*c >= 'A' && *c <= 'Z'))
+    return 1;
+  return 0;
 }
 
 char is_digit(char *c)
 {
-    if (*c >= '0' && *c <= '9')
-        return 1;
-    return 0;
+  if (*c >= '0' && *c <= '9')
+    return 1;
+  return 0;
 }
 
 char is_whitespace(char *c)
 {
-    if (*c == ' ' || *c == '\n' || *c == '\t')
-        return 1;
-    return 0;
+  if (*c == ' ' || *c == '\n' || *c == '\t')
+    return 1;
+  return 0;
 }
 
 char is_var_name_char(char *c)
 {
-    if (is_alpha(c) || *c == '_')
-        return 1;
-    if (is_alpha(c) || is_digit(c) || *c == '_')
-        return 2;
-    return 0;
+  if (is_alpha(c) || *c == '_')
+    return 1;
+  if (is_alpha(c) || is_digit(c) || *c == '_')
+    return 2;
+  return 0;
 }
 
 char is_valid_var_name(char *c)
 {
-    if (is_var_name_char(c) != 1)
-        return 0;
-    for (char *i = c + 1; *i != 0; i++)
-    {
-        if (!is_valid_var_name)
-            return 0;
-    }
-    return 1;
+  if (is_var_name_char(c) != 1)
+    return 0;
+  for (char *i = c + 1; *i != 0; i++)
+  {
+    if (!is_valid_var_name)
+      return 0;
+  }
+  return 1;
 }
 
 char str_equal(char *a, char *b)
 {
-    int i;
-    for (i = 0; a[i] != 0 && b[i] != 0; i++)
-    {
-        if (a[i] != b[i])
-            return 0;
-    }
+  int i;
+  for (i = 0; a[i] != 0 && b[i] != 0; i++)
+  {
     if (a[i] != b[i])
-        return 0;
-    return 1;
+      return 0;
+  }
+  if (a[i] != b[i])
+    return 0;
+  return 1;
 }
 
 char is_keyword(char *c)
 {
-    if (str_equal(c, "int"))
-    {
-        return 1;
-    }
-    if (str_equal(c, "char"))
-    {
-        return 2;
-    }
+  if (str_equal(c, "int"))
+  {
+    return 1;
+  }
+  if (str_equal(c, "char"))
+  {
+    return 2;
+  }
 
+  return 0;
+}
+
+typedef struct string string;
+struct string
+{
+  char *s;
+  size_t size;
+  size_t b_size;
+};
+
+static string create_string(size_t size)
+{
+  if (size < 16)
+    size = 16;
+  return (string){malloc(size), 0, size};
+}
+
+static size_t strsize(string *s)
+{
+  if (isnas(s))
     return 0;
+  return s->size;
 }
